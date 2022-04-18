6870d9d13fb9742b5b55eb1f4cfd6f042820a54e
ylfeng250 <1119030015@qq.com>
2022-04-18 22:58:20 +0800

    style:格式化

--- src/option/option.js
+++ src/option/option.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 new Vue({
-    el: '#option',
-    render: h => h(App)
-})
+	el: "#option",
+	render: (h) => h(App),
+});
